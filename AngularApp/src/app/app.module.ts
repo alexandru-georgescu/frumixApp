ea0bb52fb74d265f3c12a9b3291984f87cc5b5cc
unknown <alexbonteanu22@gmail.com>
2019-08-19 13:44:07 +0300

    v2.0

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestPageComponent } from './pages/test-page/test-page.component';
-import { UserService } from './user-service.service';
+import { UserService } from './services/user-service.service'
 
 
 
